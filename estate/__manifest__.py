b1ecfaf4995fd1f1c64b2df9fd6e2b5769709a31
oachille <oneillachille1507@gmail.com>
2025-09-16 10:00:23 +0200

    fix(manifest): Standardized Odoo manifest versioning

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Estate",  # The name that will appear in the App list
-    "version": "18.0",  # Version
+    "version": "18.0.0.0.1",  # Version
     "application": True,  # This line says the module is an App, and not a module
     "depends": ["base"],  # dependencies
     "data": [
