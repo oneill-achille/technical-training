a244f9953844f546a19614fe756cea114697748a
oachille <oneillachille1507@gmail.com>
2025-09-16 13:07:50 +0200

    feat(models): Added new field in estate model

--- estate/models/estate.py
+++ estate/models/estate.py
@@ -1,16 +1,19 @@
 from odoo import models, fields
 
+
 class Estate(models.Model):
     _name = "estate"
     _description = "Estate"
 
-    name = fields.Char(string="Name", required=True)
+    name = fields.Char(string="Name", default="Unknown")
     description = fields.Text(string="Description")
     postcode = fields.Char(string="Postcode")
-    date_availability = fields.Date(string="Date Availability")
+    date_availability = fields.Date(
+        string="Date Availability", default=fields.Datetime.now
+    )
     expected_price = fields.Float(string="Expected Price", required=True)
     selling_price = fields.Float(string="Selling Price", readonly=True, copy=False)
-    bedrooms = fields.Integer(string="Bedrooms")
+    bedrooms = fields.Integer(string="Bedrooms", default=2)
     living_area = fields.Integer(string="Living Area")
     facades = fields.Integer(string="Facades")
     garage = fields.Boolean(string="Garage")
@@ -19,3 +22,4 @@ class Estate(models.Model):
     garden_orientation = fields.Selection(
         [("north", "North"), ("south", "South"), ("east", "East"), ("west", "West")]
     )
+    active = fields.Boolean(string="Active", default=True)
