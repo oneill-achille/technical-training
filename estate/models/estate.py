bd40e940a8cc20a3898018850c1ce1a00ee9b290
oachille <oneillachille1507@gmail.com>
2025-09-16 11:41:12 +0200

    fix(views): Fixed issues with view and menus

--- estate/models/estate.py
+++ estate/models/estate.py
@@ -10,7 +10,7 @@ class Estate(models.Model):
     postcode = fields.Char(string="Postcode")
     date_availability = fields.Date(string="Date Availability")
     expected_price = fields.Float(string="Expected Price", required=True)
-    selling_price = fields.Float(string="Selling Price")
+    selling_price = fields.Float(string="Selling Price", readonly=True, copy=False)
     bedrooms = fields.Integer(string="Bedrooms")
     living_area = fields.Integer(string="Living Area")
     facades = fields.Integer(string="Facades")
