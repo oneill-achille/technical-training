edd26cf8241872561262d726db5c32827f081ba7
oachille <oneillachille1507@gmail.com>
2025-09-17 13:49:26 +0200

    fix(model): Fixed inline views errors for Property Type

--- estate/models/property_type.py
+++ estate/models/property_type.py
@@ -6,7 +6,7 @@ class PropertyType(models.Model):
     _description = "Property Type"
 
     name = fields.Char(string="Property Type", required=True)
-    property_ids = fields.One2many("estate", "property_id", string="Properties")
+    property_ids = fields.One2many("estate", "property_type_id", string="Properties")
     title = fields.Char(string="Title")
     expected_price = fields.Float(string="Expected Price")
     status = fields.Selection(
