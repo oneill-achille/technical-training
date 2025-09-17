a8af21ceb8934504ead7be84aeca99c2d9150871
oachille <oneillachille1507@gmail.com>
2025-09-17 13:44:47 +0200

    feat(model): Added inline views for Property Type

--- estate/models/property_type.py
+++ estate/models/property_type.py
@@ -6,3 +6,16 @@ class PropertyType(models.Model):
     _description = "Property Type"
 
     name = fields.Char(string="Property Type", required=True)
+    property_ids = fields.One2many("estate", "property_id", string="Properties")
+    title = fields.Char(string="Title")
+    expected_price = fields.Float(string="Expected Price")
+    status = fields.Selection(
+        [
+            ("new", "New"),
+            ("offer_received", "Offer Received"),
+            ("offer_accepted", "Offer Accepted"),
+            ("sold", "Sold"),
+            ("cancelled", "Cancelled"),
+        ],
+        string="Status",
+    )
