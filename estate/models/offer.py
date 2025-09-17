a0d1243f681d08ceb263276b6135bc9c0d196bbf
oachille <oneillachille1507@gmail.com>
2025-09-17 11:04:02 +0200

    feat(model|views): Added inverse functions in Offer model, added computed and inverse fields in Offer forms and

--- estate/models/offer.py
+++ estate/models/offer.py
@@ -1,14 +1,31 @@
-
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class Offer(models.Model):
     _name = "offer"
     _description = "Offer"
 
+    # Functions
+    @api.depends("date_deadline", "validity")
+    def _compute_date_deadline(self):
+        for record in self:
+            create_date = record.create_date or fields.Date.today()
+            record.date_deadline = fields.Date.add(create_date, days=record.validity)
+
+    def _inverse_date_deadline(self):
+        for record in self:
+            record.validity = (record.date_deadline - fields.Date.to_date(record.create_date)).days
+
+    # Attributes
     price = fields.Float(string="Price")
     status = fields.Selection(
         [("accepted", "Accepted"), ("refused", "Refused")], string="Status", copy=False
     )
     partner_id = fields.Many2one("res.partner", string="Partner", required=True)
     property_id = fields.Many2one("estate", string="Property", required=True)
+    validity = fields.Integer(string="Validity", default=7)
+    date_deadline = fields.Date(
+        string="Deadline",
+        compute="_compute_date_deadline",
+        inverse="_inverse_date_deadline",
+    )
